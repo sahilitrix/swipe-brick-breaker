7ddd9cf3270218950e39c69c29dbd927c1e7fbdc
Sahit Penmatcha <20714441+sahilitrix@users.noreply.github.com>
2020-05-01 21:16:43 -0400

    Update Game.java

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,4 @@
-/**
- * CIS 120 Game HW
- * (c) University of Pennsylvania
- * @version 2.1, Apr 2017
- */
 
-// imports necessary libraries for Java swing
 import java.awt.*;
 import java.awt.event.*;
 import java.util.TreeSet;
@@ -13,7 +7,7 @@ import javax.swing.*;
 import javax.swing.border.EmptyBorder;
 
 /**
- * Game Main class that specifies the frame and widgets of the GUI
+ * Main class to run.
  */
 public class Game implements Runnable {
 	
@@ -43,10 +37,7 @@ public class Game implements Runnable {
 			"   the list) lands.";
 	
     public void run() {
-        // NOTE : recall that the 'final' keyword notes immutability even for local variables.
 
-        // Top-level frame in which game components live
-        // Be sure to change "TOP LEVEL FRAME" to the name of your game
         final JFrame frame = new JFrame("Swipe Brick Breaker");
         frame.setLocation(300, 300);
         
@@ -75,9 +66,6 @@ public class Game implements Runnable {
         frame.add(status_panel, BorderLayout.NORTH);
 
 
-        // Note here that when we add an action listener to the reset button, we define it as an
-        // anonymous inner class that is an instance of ActionListener with its actionPerformed()
-        // method overridden. When the button is pressed, actionPerformed() will be called.
         final JButton reset = new JButton("Reset");
         reset.setFont(new Font(Font.SANS_SERIF, Font.PLAIN, 13));
         reset.addActionListener(new ActionListener() {
@@ -142,8 +130,7 @@ public class Game implements Runnable {
     }
 
     /**
-     * Main method run to start and run the game. Initializes the GUI elements specified in Game and
-     * runs it. IMPORTANT: Do NOT delete! You MUST include this in your final submission.
+     * Main method that starts the game.
      */
     public static void main(String[] args) {
         SwingUtilities.invokeLater(new Game());
